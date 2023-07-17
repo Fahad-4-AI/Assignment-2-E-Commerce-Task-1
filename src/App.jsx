0592aa3122421961301355094506c391faa07809
Fahad Ali <fali79073@gmail.com>
2023-07-16 23:41:50 -0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import axios from "axios";
+import { Route, Routes } from "react-router-dom";
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>hello world</h1>
+    </>
+  )
+}
+
+export default App
